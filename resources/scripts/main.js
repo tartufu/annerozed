c53ffb04a10aa4b493b1cec5b7c687a9fdc84440
tartufu <ainurlindale@gmail.com>
2020-09-29 12:05:56 +0800

    added lightbox gallery

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -2,5 +2,11 @@ $(document).ready(function() {
     // all custom jQuery will go here
     $(function () {
 	  $('[data-toggle="tooltip"]').tooltip()
-	})
+	});
+
+	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+	                event.preventDefault();
+	                $(this).ekkoLightbox();
+	            });
+	// https://ashleydw.github.io/lightbox/
 });
